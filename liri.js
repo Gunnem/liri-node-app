69d2906329fc28a7e0ebb4fe4e54c4c072faeb3d
Mkim <sp3dng9@gmail.com>
2018-11-27 20:35:21 -0800

    stuff added

--- liri.js
+++ liri.js
@@ -1,11 +1,171 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require("fs");
+var request = require("request");
+//var Spotify = require('spotify-web-api-node');
+var Spotify = require('node-spotify-api');
+
+//creates log.txt file
+var filename = './log.txt';
+//NPM module used to write output to console and log.txt simulatneously
+var log = require('simple-node-logger').createSimpleFileLogger(filename);
+log.setLevel('all');
+
+//argv[2] chooses users actions; argv[3] is input parameter, ie; movie title
+var command= process.argv[2];
+var commandParam = process.argv[3];
+
+//concatenate multiple words in commandParam argument
+for (var i = 4; i < process.argv.length; i++) {
+    commandParam += '+' + process.argv[i];
+}
+
+//Switch command
+function mySwitch(command) {
+
+    //choose which statement (command) to switch to and execute
+    switch (command) {
+
+        case "concert-this":
+            getConcert();
+            break;
+
+        case "spotify-this-song":
+            getSpotify();
+            break;
+
+        case "movie-this":
+            getMovie();
+            break;
+
+        case "do-what-it-says":
+            doWhat();
+            break;
+    }
+
+
+    //Bandsintown - command: concert-this
+    function getConcert() {
+        //Fetch Bandsintown Keys
+        var bandsintown = new Bandsintown(keys.bandsintown);
+        var artist = commandParam;
+        // Then run a request to the Bandsintown API with the artist specified
+        var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=BANDSINTOWN_ID";
+
+        request(queryUrl, function (error, response, body) {
+
+            // If the request is successful = 200
+            if (!error && response.statusCode === 200) {
+                var body = JSON.parse(body);
+
+                //Simultaneously output to console and log.txt via NPM simple-node-logger
+                logOutput('================ Concert Info ================');
+                logOutput("Name of the venue " + body.Title);
+                logOutput("Venue location: " + body.Year);
+                logOutput("Date of the Event  " + body.imdbRating);
+                logOutput('==================THE END=================');
+
+            } else {
+                //else - throw error
+                console.log("Error occurred.")
+            }
+        });
+    }
+// Fetch Spotify Keys
 var spotify = new Spotify(keys.spotify);
 
-// concert-this
+// Writes to the log.txt file
+var getArtistNames = function (artist) {
+    return artist.name;
+};
+// Function for running a Spotify search - Command is spotify-this-song
+var getSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "The Sign";
+    }
 
-// spotify-this-song
-// movie-this
-// do-what-it-says
+    spotify.search(
+        {
+            type: "track",
+            query: command
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+    //OMDB Movie - command: movie-this
+    function getMovie() {
+        // OMDB Movie - this MOVIE base code is from class files, I have modified for more data and assigned parse.body to a Var
+        var movieName = commandParam;
+        // Then run a request to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+        request(queryUrl, function (error, response, body) {
+
+            // If the request is successful = 200
+            if (!error && response.statusCode === 200) {
+                var body = JSON.parse(body);
+
+                //Simultaneously output to console and log.txt via NPM simple-node-logger
+                logOutput('================ Movie Info ================');
+                logOutput("Title: " + body.Title);
+                logOutput("Release Year: " + body.Year);
+                logOutput("IMdB Rating: " + body.imdbRating);
+                logOutput("Country: " + body.Country);
+                logOutput("Language: " + body.Language);
+                logOutput("Plot: " + body.Plot);
+                logOutput("Actors: " + body.Actors);
+                logOutput("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
+                logOutput("Rotten Tomatoes URL: " + body.tomatoURL);
+                logOutput('==================THE END=================');
+
+            } else {
+                //else - throw error
+                console.log("Error occurred.")
+            }
+            //Response if user does not type in a movie title
+            if (movieName === "Mr. Nobody") {
+                console.log("-----------------------");
+                console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+                console.log("It's on Netflix!");
+            }
+        });
+    }
+
+    //Function for command do-what-it-says; reads and splits random.txt file
+    //command: do-what-it-says
+    function doWhat() {
+        //Read random.txt file
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (!error);
+            console.log(data.toString());
+            //split text with comma delimiter
+            var cmds = data.toString().split(',');
+        });
+    }
+
+
+
+}   //Closes mySwitch func - Everything except the call must be within this scope
+
+//Call mySwitch function
+mySwitch(command);
+
 
 
 
